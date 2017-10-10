c8aa05690b4603b5c7aba48e0c030a95ccfe2c87
pradyumn014 <pradyumna014@gmail.com>
2017-10-10 19:52:27 +0530

    Created Models

--- Backend/interact/admin.py
+++ Backend/interact/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import news_item,AI,DB
+
+admin.site.register(news_item)
+admin.site.register(AI)
+admin.site.register(DB)
 
 # Register your models here.
--- Backend/interact/urls.py
+++ Backend/interact/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns= [
 	url(r'^check/$',views.MyResponseView.as_view()),
+	url(r'^news_item/$',views.MyResponseView.as_view()),
+	url(r'^DB/$',views.DB.as_view()),
+
 ]
